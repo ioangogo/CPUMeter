68f91fea4968389cbc053279724eac0d7370ad3c
Ioan Loosley <legit.ioangogo@gmail.com>
2016-10-21 17:51:53 +0100

    Cleaning up code and stuff

--- CPULinux.py
+++ CPULinux.py
@@ -1,36 +1,50 @@
 #!/usr/bin/env python
 from linux_metrics import cpu_stat
-import serial,gc
+import serial
+import gc
 import time
-lastnum=1
-portarray=[]
+import os
+import signal
+import sys
 import serial.tools.list_ports
+
+
+lastnum = 1
+oldnumc = 1
+lastnumarr = 1
+portarray = []
 ports = list(serial.tools.list_ports.comports())
+
 for p in ports:
     print(lastnum, p)
     portarray.append(p)
-    lastnum =+ 1
+    lastnumarray = + 1
+
 ttypath = input("Enter Aurdino tty path from above, the bit after tty: ")
-port = serial.Serial('/dev/tty'+ttypath, 9600)
-import signal
-import sys
+port = serial.Serial('/dev/tty' + ttypath, 9600)
+
 def signal_handler(signal, frame):
-        x=lastnum
-        while x>=0:
-           port.write([x])
-           time.sleep(0.001)
-           x = x - 1
-        port.write([0])
-        sys.exit(0)
+    x = lastnum
+    while x >= 0:
+        port.write([x])
+        time.sleep(0.001)
+        x = x - 1
+    port.write([0])
+    sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
+
 while True:
-#    idle = cpu_stat.cpu_percents()['idle']
-#    val = idle/100
-    val = 255 * round(100 - cpu_stat.cpu_percents()['idle'])/100
-#    val = int(round(val))
-    print(val)
-#    print("Actual: " + str(int(round(100 - cpu_stat.cpu_percents()['idle']))) + "% Data To Arduino: "+ str(val))
-    port.write(bytes([int(val)]))
+    val = 255 * round(100 - cpu_stat.cpu_percents()['idle']) / 100
+    os.system("clear")
+    print(str(round((val / 255) * 100)), "%")
+
+    if val != lastnum:
+        port.write(bytes([int(round(val))]))
+        lastnum = val
+        oldnumc = 1
+    else:
+        print("Old Number", oldnumc)
+        oldnumc += 1
 
     time.sleep(0.25)
     gc.collect()
